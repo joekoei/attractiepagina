04a5702df62db72ae8a0295d92991f2326dcf849
Joey Blommers <joeyblommers2004@gmail.com>
2021-06-04 09:23:53 +0200

    Grid :sparkles:

--- index.php
+++ index.php
@@ -25,7 +25,9 @@ require_once 'admin/backend/config.php';
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia modi dolore magnam! Iste libero voluptatum autem, sapiente ullam earum nostrum sed magnam vel laboriosam quibusdam, officia, esse vitae dignissimos nulla?
         </aside>
         <main>
-            <!-- hier komen de attractiekaartjes -->
+            <div class="grid-container">
+
+            </div>
         </main>
     </div>
 
